eae9e5c99bedc1a764795b685aa25f80db33dd35
Ori Thee <orithee@cyber4s.dev>
2022-06-29 18:20:07 +0300

    Add starter project

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+var app;
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*************************!*\
+  !*** ./dist/tsc/app.js ***!
+  \*************************/
+
+console.log('hello');
+
+app = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
