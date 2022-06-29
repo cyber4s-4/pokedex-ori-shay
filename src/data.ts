ce1adc30f7ea549423a55a27d3d2eb9df7be6686
Ori Thee <orithee@cyber4s.dev>
2022-06-29 18:49:07 +0300

    Add data file

---
+++ src/data.ts
@@ -0,0 +1,5 @@
+// TODO: get the api from the websites of the pokimons...
+// TODO: build component of pokemon file
+// TODO: Add hyper link to all the pokemons
+// TODO: Add search machine that search all the pokemons and return the selected search..
+// TODO:
