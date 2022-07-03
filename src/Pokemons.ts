ce74d11b8d30675711bf34d12d33a874c39cb318
Ori Thee <orithee@cyber4s.dev>
2022-07-03 08:57:23 +0300

    Fix bug - delete the 'temp'. just 'counter'

--- src/Pokemons.ts
+++ src/Pokemons.ts
@@ -1,31 +1,37 @@
-import { BUTTON_INPUT, FIRST_CONTAINER, MAIN_CONTAINER, SEARCH_INPUT } from "./app"
-import { AllPokesComponent } from "./AllPokesComponent"
-import { Data, extractPokemon, getPokemons } from "./data"
-import { PokemonComponent } from "./pokemonComponent"
-import { closeButtonFunc } from "./buttons"
+import {
+  BUTTON_INPUT,
+  FIRST_CONTAINER,
+  MAIN_CONTAINER,
+  SEARCH_INPUT,
+} from './app';
+import { AllPokesComponent } from './AllPokesComponent';
+import { Data, extractPokemon, getPokemons } from './data';
+import { PokemonComponent } from './pokemonComponent';
+import { closeButtonFunc } from './buttons';
 
-const POKEMON_STEPS = 15 // Each scroll the page uploads 15 pokemons.
-export let counter = 0
+const POKEMON_STEPS = 15; // Each scroll the page uploads 15 pokemons.
+export let counter = 0;
 
 /**
  * The function add pokemons to the main page when it upload.
  */
 export async function addPokemons(): Promise<void> {
-  const POKEMON_DATA = await getPokemons()
-  const poke = POKEMON_DATA.pokemon_entries
+  const POKEMON_DATA = await getPokemons();
+  const poke = POKEMON_DATA.pokemon_entries;
 
-  let temp = counter
-  for (let i = counter; i < POKEMON_STEPS + temp; i++) {
-    const specificPokemon = await extractPokemon(poke[i].pokemon_species.name)
+  for (let i = 0; i < POKEMON_STEPS; i++) {
+    const specificPokemon = await extractPokemon(
+      poke[counter].pokemon_species.name
+    );
     const elementData: Data = {
-      name: poke[i].pokemon_species.name,
+      name: poke[counter].pokemon_species.name,
       img: specificPokemon.sprites.front_default,
       height: specificPokemon.height,
       weight: specificPokemon.weight,
-      id: poke[i].entry_number,
-    }
-    counter++
-    new AllPokesComponent(elementData, FIRST_CONTAINER).render()
+      id: poke[counter].entry_number,
+    };
+    counter++;
+    new AllPokesComponent(elementData, FIRST_CONTAINER).render();
   }
 }
 
@@ -35,31 +41,31 @@ export async function addPokemons(): Promise<void> {
  * @param {any} element - An object with the pokemon data
  */
 export async function viewPokemon(element: any): Promise<void> {
-  MAIN_CONTAINER.style.display = "block"
-  MAIN_CONTAINER.innerHTML = ""
-  const specificPokemon = await extractPokemon(SEARCH_INPUT.value)
+  MAIN_CONTAINER.style.display = 'block';
+  MAIN_CONTAINER.innerHTML = '';
+  const specificPokemon = await extractPokemon(SEARCH_INPUT.value);
   const elementData: Data = {
     name: element.pokemon_species.name,
     img: specificPokemon.sprites.front_default,
     height: specificPokemon.height,
     weight: specificPokemon.weight,
     id: element.entry_number,
-  }
-  new PokemonComponent(elementData, MAIN_CONTAINER).render()
+  };
+  new PokemonComponent(elementData, MAIN_CONTAINER).render();
 }
 
 /**
  * Function that returns a no result message - if there is no pokemon
  */
 export function noResults(): void {
-  MAIN_CONTAINER.style.display = "block"
-  MAIN_CONTAINER.innerHTML = ""
+  MAIN_CONTAINER.style.display = 'block';
+  MAIN_CONTAINER.innerHTML = '';
 
-  const h1Element = document.createElement("h1") as HTMLElement
-  h1Element.innerHTML = `There is no ${SEARCH_INPUT.value} Pokemon... try another one!`
-  MAIN_CONTAINER.appendChild(h1Element)
+  const h1Element = document.createElement('h1') as HTMLElement;
+  h1Element.innerHTML = `There is no ${SEARCH_INPUT.value} Pokemon... try another one!`;
+  MAIN_CONTAINER.appendChild(h1Element);
 
-  closeButtonFunc(MAIN_CONTAINER, MAIN_CONTAINER)
+  closeButtonFunc(MAIN_CONTAINER, MAIN_CONTAINER);
 }
 
 /**
@@ -67,11 +73,12 @@ export function noResults(): void {
  * If there is a pokemon - it initiate viewPokemon function. else initiate noResult function.
  */
 export function searchInputFunc(): void {
-  BUTTON_INPUT.addEventListener("click", async () => {
-    const POKEMON_DATA = await getPokemons()
+  BUTTON_INPUT.addEventListener('click', async () => {
+    const POKEMON_DATA = await getPokemons();
     POKEMON_DATA.pokemon_entries.forEach((element: any) => {
-      if (SEARCH_INPUT.value === element.pokemon_species.name) viewPokemon(element)
-    })
-    if (MAIN_CONTAINER.style.display === "none") noResults()
-  })
+      if (SEARCH_INPUT.value === element.pokemon_species.name)
+        viewPokemon(element);
+    });
+    if (MAIN_CONTAINER.style.display === 'none') noResults();
+  });
 }
