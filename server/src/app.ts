50cd6c0965c3f701fb78407a50f2cd8eaf390dbf
Ori Thee <orithee@cyber4s.dev>
2022-07-08 02:34:35 +0300

    Refactor - clean the code

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,9 +16,8 @@ interface User {
   avatar: string;
   id: string;
 }
-console.log('start server-file');
-const filePath: string = path.join(__dirname, '../data/data.json');
-const filePath2: string = path.join(__dirname, '../data/data2.json');
+console.log('start app.ts - server - file');
+const pathToDataJson: string = path.join(__dirname, '../data/data.json');
 
 function initServer() {
   app.get('*', (req: Request, res: Response) => {
@@ -27,49 +26,21 @@ function initServer() {
   });
 
   if (!false) {
-    // insertToDataJson1();
     insertToDataJson2();
   }
   app.listen(3000, () => console.log('listening to port 3000'));
 }
 
-async function insertToDataJson1() {
-  const POKEMON_DATA: any = await getPokemons();
-  const POKE_LIST = POKEMON_DATA.pokemon_entries;
-  fs.writeFileSync(filePath, JSON.stringify(POKE_LIST));
-  console.log(POKE_LIST.length);
-}
-
 initServer();
 
-// Shay : this is the trial of the forEach 'Fetch' 1000 times :)
-// async function insertToDataJson2() {
-//   const POKEMON_DATA: any = await getPokemons();
-//   const POKE_LIST2: (any | undefined)[] = [];
-//   // await console.log(POKEMON_DATA.pokemon_entries);
-//   await POKEMON_DATA.pokemon_entries.forEach(async (el: any) => {
-//     try {
-//       const newPokemon = await extractPokemon(el.pokemon_species.name);
-//       console.log(POKE_LIST2.length);
-//       await POKE_LIST2.push(newPokemon);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
-//   await console.log(POKE_LIST2);
-//   await console.log('end Data2');
-//   // await fs.writeFileSync(filePath2, JSON.stringify(POKE_LIST));
-//   // await console.log(POKE_LIST);
-// }
-// }
 async function insertToDataJson2() {
-  const POKE_LIST2: (any | undefined)[] = [];
+  const dataList: (any | undefined)[] = [];
 
   for (let i = 1; i < 400; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
       .then((res) => res.json())
       .then((res) => {
-        POKE_LIST2.push({
+        dataList.push({
           id: res.id,
           name: res.name,
           weight: res.weight,
@@ -77,6 +48,6 @@ async function insertToDataJson2() {
           image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`,
         });
       })
-      .then(() => fs.writeFileSync(filePath2, JSON.stringify(POKE_LIST2)));
+      .then(() => fs.writeFileSync(pathToDataJson, JSON.stringify(dataList)));
   }
 }
