1425c46d96480636808b0ec5e74670f76c77a16c
Ori Thee <orithee@cyber4s.dev>
2022-07-07 16:29:05 +0300

    Try to do the 'extractPokemon' fetch

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import { json } from 'body-parser';
-import { getPokemons } from './data';
-// import { ll } from '../../client/src/';
+import { getPokemons, extractPokemon } from './data';
 
 const fs = require('fs');
 const path = require('path');
@@ -18,23 +17,42 @@ interface User {
 }
 console.log('start server-file');
 const filePath: string = path.join(__dirname, '../data/data.json');
+const filePath2: string = path.join(__dirname, '../data/data2.json');
+
 function initServer() {
   app.get('*', (req: Request, res: Response) => {
     console.log('Processing request: ', req.url);
     res.sendFile(req.path || 'index.html', { root: './../client/dist' });
   });
 
+  // insertToDataJson2();
+
   if (false) {
-    insertToDataJson();
+    insertToDataJson1();
   }
   app.listen(3000, () => console.log('listening to port 3000'));
 }
 
-async function insertToDataJson() {
+async function insertToDataJson1() {
   const POKEMON_DATA: any = await getPokemons();
   const POKE_LIST = POKEMON_DATA.pokemon_entries;
   fs.writeFileSync(filePath, JSON.stringify(POKE_LIST));
-  console.log(POKE_LIST);
 }
 
 initServer();
+
+// Shay : this is the trial of the forEach 'Fetch' 1000 times :)
+// async function insertToDataJson2() {
+//   const readFileData: any[] = await JSON.parse(
+//     fs.readFileSync(filePath, 'utf8')
+//   );
+//   const POKE_LIST: (any | undefined)[] = [];
+//   await readFileData.forEach(async (el) => {
+//     const newPokemon = await extractPokemon(el.pokemon_species.name);
+//     console.log();
+//     await POKE_LIST.push(newPokemon);
+//   });
+//   await console.log(POKE_LIST);
+//   await fs.writeFileSync(filePath2, JSON.stringify(POKE_LIST));
+//   await console.log(POKE_LIST);
+// }
