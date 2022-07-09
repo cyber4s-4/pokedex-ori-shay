9115c5b44522b807ce8d1783682759432e258278
Ori Thee <orithee@cyber4s.dev>
2022-07-09 21:25:16 +0300

    fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,53 +1,60 @@
-import express from "express"
-import { Request, Response } from "express"
-import { json } from "body-parser"
-import { Data, dataList, insertToDataJson } from "./data"
+import express from 'express';
+import { Request, Response } from 'express';
+import { json } from 'body-parser';
+import { Data, dataList, insertToDataJson } from './data';
 
-export const fs = require("fs")
-const path = require("path")
+export const fs = require('fs');
+const path = require('path');
 
-const app = express()
-app.use(json())
-app.use(express.static("./../client/dist"))
+const app = express();
+app.use(json());
+app.use(express.static('./../client/dist'));
 
-export const pathDataJson: string = path.join(__dirname, "../data/data.json")
-const readFileData: Data[] = fs.readFileSync(pathDataJson, "utf8")
+export const pathDataJson: string = path.join(__dirname, '../data/data.json');
+const readFileData: string = fs.readFileSync(pathDataJson, 'utf8');
 
 /**
  * The function Init the server on port 3000, and get data from /get-data.
  *
  */
 async function initServer() {
-  await insertToDataJson()
-  console.log('The "data.json" file is ready. Starting to run the server: ')
+  await insertToDataJson(readFileData);
+  console.log('The "data.json" file is ready. Starting to run the server: ');
 
-  await continueInit()
+  await continueInit();
   function continueInit() {
-    app.get("/", (req: Request, res: Response) => {
-      console.log("Processing request: ", req.url)
-      res.sendFile(req.path || "index.html", { root: "./../client/dist" })
-    })
+    app.get('/', (req: Request, res: Response) => {
+      console.log('Processing request: ', req.url);
+      res.sendFile(req.path || 'index.html', { root: './../client/dist' });
+    });
 
-    app.get("/get-data", (req: Request, res: Response) => {
-      res.sendFile("data.json", { root: "./../server/data" })
-    })
-    app.listen(3000, () => console.log("listening to port 3000"))
+    app.get('/get-data', (req: Request, res: Response) => {
+      res.sendFile('data.json', { root: './../server/data' });
+    });
+
+    app.post('/:name', (req: Request, res: Response) => {
+      const name = req.params.name;
+      const name2 = req.query.name;
+      console.log(name);
+      console.log(name2);
+      console.log(readFileData);
+      // const findPokemon = JSON.parse(readFileData).find(
+      //   (el) => el.name === name
+      // );
+      // if (findPokemon) {
+      //   const newArr = readFileData.filter((el) => el.name !== name);
+      //   fs.writeFileSync(pathDataJson, JSON.stringify(newArr));
+      //   res.send(newArr);
+      // } else {
+      //   res.send('No find name');
+      // }
+    });
+
+    app.listen(3000, () => console.log('listening to port 3000'));
   }
 }
 
-app.post("/:name", (req: Request, res: Response) => {
-  const name = req.params.name
-  const findPokemon = readFileData.find((el) => el.name === name)
-  if (findPokemon) {
-    const newArr = readFileData.filter((el) => el.name !== name)
-    fs.writeFileSync(pathDataJson, JSON.stringify(newArr))
-    res.send(newArr)
-  } else {
-    res.send("No find name")
-  }
-})
-
-initServer()
+initServer();
 
 // const dataList: Data[] = [];
 // async function insertToDataJson() {
--- server/src/data.ts
+++ server/src/data.ts
@@ -1,22 +1,22 @@
-import fetch from "cross-fetch"
-import { fs, pathDataJson } from "./app"
+import fetch from 'cross-fetch';
+import { fs, pathDataJson } from './app';
 export interface Data {
-  name: string
-  img: string
-  height: string
-  weight: string
-  id: number
-  favorite: boolean
+  name: string;
+  img: string;
+  height: string;
+  weight: string;
+  id: number;
+  favorite: boolean;
 }
 
-export const dataList: Data[] = []
+export const dataList: Data[] = [];
 
 /**
  *
  * The function insert data to json file when the server created.
  */
-export async function insertToDataJson(): Promise<void> {
-  if (insertToDataJson.length !== 0) return
+export async function insertToDataJson(dataFile: string): Promise<void> {
+  if (dataFile !== '') return;
 
   for (let i = 1; i <= 898; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
@@ -29,14 +29,16 @@ export async function insertToDataJson(): Promise<void> {
           weight: res.weight,
           id: res.id,
           favorite: false,
-        })
+        });
       })
       .then(() => {
-        dataList.sort((a: Data, b: Data) => a.id - b.id)
-        fs.writeFileSync(pathDataJson, JSON.stringify(dataList))
+        dataList.sort((a: Data, b: Data) => a.id - b.id);
+        fs.writeFileSync(pathDataJson, JSON.stringify(dataList));
       })
       .catch(() =>
-        console.log(`Error:  fetch fail in  https://pokeapi.co/api/v2/pokemon/${i}`)
-      )
+        console.log(
+          `Error:  fetch fail in  https://pokeapi.co/api/v2/pokemon/${i}`
+        )
+      );
   }
 }
