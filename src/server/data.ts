626436bc7b897266116cbb5721114b46f5f51ae1
Ori Thee <orithee@cyber4s.dev>
2022-07-22 11:10:51 +0300

    Delete function

--- src/server/data.ts
+++ src/server/data.ts
@@ -1,6 +1,3 @@
-import fetch from 'cross-fetch';
-import { Collection } from 'mongodb';
-
 export interface Data {
   name: string;
   img: string;
@@ -9,41 +6,6 @@ export interface Data {
   id: number;
   favorite: boolean;
   id_serial?: number;
-}
-
-export const dataList: Data[] = [];
-
-/**
- * THE FUNCTION IS NOT IN USE!
- * The function insert data to JSON database from API and sort it by id.
- *
- * @param {string} dataFile - An object with the pokemon data
- */
-export async function insertToDataJson(dataFile: string): Promise<void> {
-  if (dataFile !== '') return;
-
-  for (let i = 1; i <= 898; i++) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-      .then((res) => res.json())
-      .then((res) => {
-        dataList.push({
-          name: res.name,
-          img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`,
-          height: res.height,
-          weight: res.weight,
-          id: res.id,
-          favorite: false,
-        });
-      })
-      .then(() => {
-        dataList.sort((a: Data, b: Data) => a.id - b.id);
-      })
-      .catch(() => {
-        console.log(
-          `Error:  fetch fail in  https://pokeapi.co/api/v2/pokemon/${i}`
-        );
-      });
-  }
 }
 
 /**
