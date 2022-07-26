fda3b4c4c7c25b7e40c9d1ec9f280459c0bb94fd
shay-sagzan <shay@cyber4s.dev>
2022-07-26 16:09:01 +0300

    env files

---
+++ src/server/environment.d.ts
@@ -0,0 +1,11 @@
+export {};
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: string;
+      PORT: string;
+      MONGO_DB_URL: string;
+      DATABASE_URL: string;
+    }
+  }
+}
