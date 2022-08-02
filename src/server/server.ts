4379287c745f5886de692130365ec0177224d05c
Ori Thee <orithee@cyber4s.dev>
2022-08-02 17:32:10 +0300

    Change the 'app' to 'server' + refactoring

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,4 @@
+import { getPokemonsFromApi } from './data';
 import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 import {
@@ -8,24 +9,22 @@ import {
   updateFavorites,
 } from './connect';
 // } from './mongo';
-import { getPokemonsFromApi } from './data';
 
 import dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/.env' });
 
-const app = express();
-app.use(json());
-app.use(express.static('./dist'));
-
 const validation = require('./validation');
-app.use('/validation', validation);
+const server = express();
+
+server.use(json());
+server.use(express.static('./dist'));
+server.use('/validation', validation);
 
 init();
 
 /**
  * The function connects the database and build the table with the pokemon's.
  * Then the function load the server his functions included.
- *
  */
 async function init() {
   await clientConnect();
@@ -35,26 +34,25 @@ async function init() {
 
 /**
  * The function load the functions of the server
- *
  */
 async function loadServer() {
   // Get 20 pokemon's from the database from counter number
-  app.get('/get20Pokemons/:counter', async (req: Request, res: Response) => {
+  server.get('/get20Pokemons/:counter', async (req: Request, res: Response) => {
     res.send(await get20Pokemons(Number(req.params.counter)));
   });
 
   // Get a specific pokemon from database
-  app.get('/get-specific/:specific', async (req: Request, res: Response) => {
+  server.get('/get-specific/:specific', async (req: Request, res: Response) => {
     res.send(await getSpecificPoke(req.params.specific));
   });
 
   // Update if pokemon is marked as favorite or not
-  app.post('/star', async (req: Request, res: Response) => {
+  server.post('/star', async (req: Request, res: Response) => {
     await updateFavorites(req.body.name, req.body.favoritePoke);
   });
 
   const port = process.env.PORT;
-  app.listen(port, () => console.log('Hosted: http://localhost:' + port));
+  server.listen(port, () => console.log('Hosted: http://localhost:' + port));
 }
 
 // TODO: Ori:
@@ -79,4 +77,6 @@ async function loadServer() {
 // - Testing - see the task-03 js in the 10-testing solutions (ori)  - way to check requests...
 // - When i working on the auth - i need try use 1 file of html + 1 file of typesrcipt -
 // that every stage its change the body of the html...
-// Fix the css ! specific on the validation.scss...
+// - Fix the css ! specific on the validation.scss...
+// - Check the response... in the android..
+// - favicon
