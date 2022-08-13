e47d13d2a5c471cb8d30580e73f4f386a053337c
Ori Thee <orithee@cyber4s.dev>
2022-08-13 22:44:28 +0300

    Fix the init bug

--- src/client/app.ts
+++ src/client/app.ts
@@ -7,21 +7,22 @@ export const FIRST_CONTAINER = getDivElement('first-container');
 export const MAIN_CONTAINER = getDivElement('sub-container');
 export const BUTTON_INPUT = getInputElement('button-input');
 export const SEARCH_INPUT = getButtonElement('search-input');
-MAIN_CONTAINER.style.display = 'none';
 
 init();
 
 function init() {
   axios.get(location.origin + '/validation/init').then((res) => {
     if (res.data.message) {
-      onload = (event) => {
-        console.log('load');
-        scrolling();
-        searchInputFunc();
-        buttons();
-      };
+      scrolling();
+      searchInputFunc();
+      buttons();
+      MAIN_CONTAINER.style.display = 'none';
+      const loader = document.querySelector(
+        '.loader-background'
+      ) as HTMLDivElement;
+      loader.style.display = 'none';
     } else {
-      console.log(res.data.message);
+      console.log('message: ' + res.data.message);
       window.location.pathname = '/validation/login';
     }
   });
